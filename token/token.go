46c3d4adff582d1701da5497c85a432922ac1b55
Rishi Kothari <itsrishikothari@gmail.com>
2020-11-18 17:01:57 -0500

    remove token

--- token/token.go
+++ token/token.go
@@ -1,32 +1,7 @@
 package token
 
-//Type is a token type
-type Type string
-
 // Token is a token
 type Token struct {
 	Type    Type
 	Literal string
 }
-
-const (
-	// Endings
-	ILLEGAL = "ILLEGAL"
-	EOF     = "EOF"
-	// Identifiers + literals
-	IDENT = "IDENT" // add, foobar, x, y, ...
-	INT   = "INT"   // 1343456
-	// Operators
-	ASSIGN = "="
-	PLUS   = "+"
-	// Delimiters
-	COMMA     = ","
-	SEMICOLON = ";"
-	LPAREN    = "("
-	RPAREN    = ")"
-	LBRACE    = "{"
-	RBRACE    = "}"
-	// Keywords
-	FUNCTION = "FUNCTION"
-	LET      = "LET"
-)
