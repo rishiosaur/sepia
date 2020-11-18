179132e3628596322b5d6af428840bdf23afadcd
Rishi Kothari <itsrishikothari@gmail.com>
2020-11-18 17:01:53 -0500

    new keywords

--- token/keywords.go
+++ token/keywords.go
@@ -1,8 +1,13 @@
 package token
 
 var keywords = map[string]Type{
-	"fn":  FUNCTION,
-	"let": LET,
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
 }
 
 //LookupIdent finds an identifier token type from a string.
