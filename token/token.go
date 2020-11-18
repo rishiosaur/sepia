c981456afc3a65498591ab452ac0a936893c6635
Rishi Kothari <itsrishikothari@gmail.com>
2020-11-18 13:36:05 -0500

    token types

---
+++ token/token.go
@@ -0,0 +1,30 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	// Endings
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+	// Identifiers + literals
+	IDENT = "IDENT" // add, foobar, x, y, ...
+	INT   = "INT"   // 1343456
+	// Operators
+	ASSIGN = "="
+	PLUS   = "+"
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+	LPAREN    = "("
+	RPAREN    = ")"
+	LBRACE    = "{"
+	RBRACE    = "}"
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
