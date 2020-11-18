957183f38b6a203ebb391eef6301222a9e31bcd3
Rishi Kothari <itsrishikothari@gmail.com>
2020-11-18 17:01:47 -0500

    lexer test (V BROKEN DO NOT RUN)

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -7,12 +7,15 @@ import (
 )
 
 func TestNextToken(t *testing.T) {
-	input := `let five = 5;
+	input :=
+		`let five = 5;
 let ten = 10;
 let add = fn(x, y) {
-x + y;
+	x + y;
 };
 let result = add(five, ten);
+5 < 10 > 5;
+!-/*5;
 `
 	tests := []struct {
 		expectedType    token.Type
