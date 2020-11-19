da02cfc3b095d68936bc0beb15b6fd76a6b535f4
Rishi Kothari <itsrishikothari@gmail.com>
2020-11-19 11:03:34 -0500

    repl

--- repl/repl.go
+++ repl/repl.go
@@ -23,7 +23,7 @@ func Start(in io.Reader, out io.Writer) {
 		line := scanner.Text()
 
 		l := lexer.New(line)
-		for tok := l.nextToken(); tok.Type != token.EOF; tok = l.nextToken() {
+		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
 			fmt.Printf("%+v\n", tok)
 		}
 	}
