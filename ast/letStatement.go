855ca234682b50dcfc823b934a40435b45819335
Rishi Kothari <itsrishikothari@gmail.com>
2020-11-19 12:02:28 -0500

    let

--- ast/letStatement.go
+++ ast/letStatement.go
@@ -1,6 +1,9 @@
 package ast
 
-import "monkey-go/token"
+import (
+	"bytes"
+	"monkey-go/token"
+)
 
 type LetStatement struct {
 	Token token.Token
@@ -10,3 +13,18 @@ type LetStatement struct {
 
 func (ls *LetStatement) statementNode()       {}
 func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+func (ls *LetStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(ls.TokenLiteral() + " ")
+	out.WriteString(ls.Name.String())
+	out.WriteString(" - ")
+
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
+}
