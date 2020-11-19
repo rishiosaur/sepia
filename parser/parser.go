22985f1efbff057e9176cc67ad7686600a1cdec1
Rishi Kothari <itsrishikothari@gmail.com>
2020-11-19 11:03:29 -0500

    parser

---
+++ parser/parser.go
@@ -0,0 +1,107 @@
+package parser
+
+import (
+	"fmt"
+	"monkey-go/ast"
+	"monkey-go/lexer"
+	"monkey-go/token"
+)
+
+type Parser struct {
+	lexer *lexer.Lexer
+
+	currentToken token.Token
+	readToken    token.Token
+
+	errors []string
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{lexer: l,
+		errors: []string{}}
+
+	p.consumeToken()
+	p.consumeToken()
+
+	return p
+}
+
+func (p *Parser) Errors() []string {
+	return p.errors
+}
+
+func (p *Parser) consumeToken() {
+	p.currentToken = p.readToken
+	p.readToken = p.lexer.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	program := &ast.Program{}
+	program.Statements = []ast.Statement{}
+
+	for p.currentToken.Type != token.EOF {
+		_statement := p.parseStatement()
+
+		if _statement != nil {
+			program.Statements = append(program.Statements, _statement)
+		}
+
+		p.consumeToken()
+	}
+
+	return program
+
+}
+
+func (p *Parser) parseStatement() ast.Statement {
+	switch p.currentToken.Type {
+	case token.LET:
+		return p.parseLetStatement()
+
+	default:
+		return nil
+	}
+}
+
+func (p *Parser) parseLetStatement() *ast.LetStatement {
+	stmt := &ast.LetStatement{Token: p.currentToken}
+
+	if !p.expectPeek(token.IDENT) {
+		return nil
+	}
+
+	stmt.Name = &ast.Identifier{Token: p.currentToken, Value: p.currentToken.Literal}
+
+	if !p.expectPeek(token.ASSIGN) {
+		return nil
+	}
+	// TODO: parse expressions
+	for !p.currentTokenIs(token.SEMICOLON) {
+		p.consumeToken()
+	}
+
+	return stmt
+}
+
+func (p *Parser) currentTokenIs(tok token.Type) bool {
+	return p.currentToken.Type == tok
+}
+
+func (p *Parser) peekTokenIs(tok token.Type) bool {
+	return p.readToken.Type == tok
+}
+
+func (p *Parser) addPeekError(tok token.Type) {
+	msg := fmt.Sprintf("Expected next token to be %s, got %s instead", tok, p.readToken)
+	p.errors = append(p.errors, msg)
+}
+
+func (p *Parser) expectPeek(tok token.Type) bool {
+	if p.peekTokenIs(tok) {
+		p.consumeToken()
+		return true
+	} else {
+		p.addPeekError(tok)
+		return false
+	}
+}
