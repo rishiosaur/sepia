0095922e5c4f5de8b3db6497d20de1371ae25006
Rishi Kothari <itsrishikothari@gmail.com>
2020-11-19 11:10:59 -0500

    add return statement types

--- ast/ast.go
+++ ast/ast.go
@@ -25,6 +25,14 @@ type LetStatement struct {
 func (ls *LetStatement) statementNode()       {}
 func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
 
+type ReturnStatement struct {
+	Token       token.Token
+	ReturnValue Expression
+}
+
+func (rs *ReturnStatement) statementNode()       {}
+func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
+
 type Identifier struct {
 	Token token.Token
 	Value string
