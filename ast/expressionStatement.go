389017000c7fc5c73f1c6d51aa00a09a07a20b2d
Rishi Kothari <itsrishikothari@gmail.com>
2020-11-19 12:02:36 -0500

    stuff for ast

---
+++ ast/expressionStatement.go
@@ -0,0 +1,18 @@
+package ast
+
+import "monkey-go/token"
+
+type ExpressionStatement struct {
+	Token      token.Token
+	Expression Expression
+}
+
+func (es *ExpressionStatement) statementNode()       {}
+func (es *ExpressionStatement) TokenLiteral() string { return es.Token.Literal }
+
+func (es *ExpressionStatement) String() string {
+	if es.Expression != nil {
+		return es.Expression.String()
+	}
+	return ""
+}
---
+++ ast/integerLiteral.go
@@ -0,0 +1,12 @@
+package ast
+
+import "monkey-go/token"
+
+type IntegerLiteral struct {
+	Token token.Token
+	Value int
+}
+
+func (il *IntegerLiteral) expressionNode()      {}
+func (il *IntegerLiteral) TokenLiteral() string { return il.Token.Literal }
+func (il *IntegerLiteral) String() string       { return il.Token.Literal }
