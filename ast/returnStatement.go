83e197b1f54dc341cd17d7061e5eeefac9573874
Rishi Kothari <itsrishikothari@gmail.com>
2020-11-19 12:02:29 -0500

    return

--- ast/returnStatement.go
+++ ast/returnStatement.go
@@ -1,6 +1,9 @@
 package ast
 
-import "monkey-go/token"
+import (
+	"bytes"
+	"monkey-go/token"
+)
 
 type ReturnStatement struct {
 	Token       token.Token
@@ -9,3 +12,12 @@ type ReturnStatement struct {
 
 func (rs *ReturnStatement) statementNode()       {}
 func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
+func (rs *ReturnStatement) String() string {
+	var out bytes.Buffer
+	out.WriteString(rs.TokenLiteral() + " ")
+	if rs.ReturnValue != nil {
+		out.WriteString(rs.ReturnValue.String())
+	}
+	out.WriteString(";")
+	return out.String()
+}
