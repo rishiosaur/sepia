8fa30b1efcea62bb720411f95e9a90827cd28972
Rishi Kothari <itsrishikothari@gmail.com>
2020-11-19 12:02:24 -0500

    idetifier type

--- ast/ident.go
+++ ast/ident.go
@@ -9,3 +9,4 @@ type Identifier struct {
 
 func (i *Identifier) expressionNode()      {}
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+func (i *Identifier) String() string       { return i.Value }
