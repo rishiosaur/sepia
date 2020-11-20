6385b055e4f064a2e02835dd437b1522be5d5728
Rishi Kothari <itsrishikothari@gmail.com>
2020-11-20 09:33:36 -0500

    repl

--- repl/repl.go
+++ repl/repl.go
@@ -5,16 +5,16 @@ import (
 	"fmt"
 	"io"
 	"monkey-go/lexer"
-	"monkey-go/token"
+	"monkey-go/parser"
 )
 
-const PROMPT_START = "#> "
+const prompt string = "#> "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
 
 	for {
-		fmt.Printf(PROMPT_START)
+		fmt.Printf(prompt)
 		scanned := scanner.Scan()
 		if !scanned {
 			return
@@ -23,8 +23,16 @@ func Start(in io.Reader, out io.Writer) {
 		line := scanner.Text()
 
 		l := lexer.New(line)
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
-		}
+		// for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
+		// 	fmt.Printf("%+v\n", tok)
+		// }
+
+		p := parser.New(l)
+
+		program := p.ParseProgram().String()
+
+		// if len(p.Errors()) == 0 {
+		fmt.Println(program)
+		// }
 	}
 }
