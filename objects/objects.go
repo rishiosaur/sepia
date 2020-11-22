3f056d1370ac2a1d3828ca30df28bc229d11ba04
Rishi Kothari <itsrishikothari@gmail.com>
2020-11-22 17:12:51 -0500

    objects

--- objects/objects.go
+++ objects/objects.go
@@ -1,15 +1,21 @@
 package objects
 
 import (
+	"bytes"
 	"fmt"
+	"sepia/ast"
+	"strings"
 )
 
 type ObjectType string
 
 const (
-	INTEGER_OBJ = "INTEGER"
-	BOOLEAN_OBJ = "BOOLEAN"
-	NULL_OBJ    = "NULL"
+	INTEGER_OBJ      = "INTEGER"
+	BOOLEAN_OBJ      = "BOOLEAN"
+	NULL_OBJ         = "NULL"
+	RETURN_VALUE_OBJ = "RETURN_VALUE"
+	ERROR_OBJ        = "ERROR"
+	FUNCTION_OBJ     = "FUNCTION"
 )
 
 type Object interface {
@@ -35,3 +41,70 @@ type Null struct{}
 
 func (n *Null) Type() ObjectType { return NULL_OBJ }
 func (n *Null) Inspect() string  { return "null" }
+
+type ReturnValue struct {
+	Value Object
+}
+
+func (rv *ReturnValue) Type() ObjectType { return RETURN_VALUE_OBJ }
+func (rv *ReturnValue) Inspect() string  { return rv.Value.Inspect() }
+
+type Error struct {
+	Message string
+}
+
+func (e *Error) Type() ObjectType { return ERROR_OBJ }
+func (e *Error) Inspect() string  { return "ERROR: " + e.Message }
+
+type Machine struct {
+	store map[string]Object
+	outer *Machine
+}
+
+func NewMachine() *Machine {
+	s := make(map[string]Object)
+	return &Machine{store: s, outer: nil}
+}
+
+func NewLocalMachine(outer *Machine) *Machine {
+	env := NewMachine()
+	env.outer = outer
+	return env
+}
+
+func (e *Machine) Get(name string) (Object, bool) {
+	obj, ok := e.store[name]
+	if !ok && e.outer != nil {
+		obj, ok := e.outer.Get(name)
+		return obj, ok
+	}
+	return obj, ok
+}
+
+func (e *Machine) Set(name string, val Object) Object {
+	e.store[name] = val
+	return val
+}
+
+type Function struct {
+	Parameters []*ast.Identifier
+	Body       *ast.BlockStatement
+	Machine    *Machine
+}
+
+func (f *Function) Type() ObjectType { return FUNCTION_OBJ }
+func (f *Function) Inspect() string {
+	var out bytes.Buffer
+
+	params := []string{}
+	for _, p := range f.Parameters {
+		params = append(params, p.String())
+	}
+	out.WriteString("fn")
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(") {\n")
+	out.WriteString(f.Body.String())
+	out.WriteString("\n}")
+	return out.String()
+}
