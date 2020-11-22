1a557ada74c3aecf2e02326c161b9d02cdd940ba
Rishi Kothari <itsrishikothari@gmail.com>
2020-11-22 14:20:14 -0500

    repl updates

--- repl/repl.go
+++ repl/repl.go
@@ -8,33 +8,30 @@ import (
 	"sepia/parser"
 )
 
-const prompt string = "#> "
+const prompt string = "⟖ "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
-
 	for {
 		fmt.Printf(prompt)
 		scanned := scanner.Scan()
 		if !scanned {
 			return
 		}
-
 		line := scanner.Text()
-
 		l := lexer.New(line)
-		// for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-		// 	fmt.Printf("%+v\n", tok)
-		// }
-
 		p := parser.New(l)
-
 		program := p.ParseProgram()
-
-		if len(p.Errors()) == 0 {
-			fmt.Println(program.Statements)
-		} else {
-			fmt.Println(fmt.Sprintf("%#v", p.Errors()))
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+		io.WriteString(out, "✅ "+program.String())
+		io.WriteString(out, "\n")
+	}
+}
+func printParserErrors(out io.Writer, errors []string) {
+	for _, msg := range errors {
+		io.WriteString(out, "❌ ERROR: "+msg+"\n")
 	}
 }
