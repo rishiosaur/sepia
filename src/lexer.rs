ee9ae28283d544cbfac1d49a3017656d3877ae37
Rishi Kothari <itsrishikothari@gmail.com>
2020-11-28 11:55:53 -0500

    add new lexer

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,300 +1,272 @@
-use std::collections::HashMap;
-use std::fmt;
+#[derive(Debug, Clone, Copy)]
+pub struct Position {
+    column: usize,
+    line: usize
+}
 
-use fmt::Formatter;
+#[derive(Debug, Clone)]
+pub enum TokenType {
+    //Associated 
+    String(String),
+    Integer(usize),
+    Float(f64),
+    Identifier(String),
 
-lazy_static! {
-    static ref KEYWORDS: HashMap<&'static str, TokenType> = {
-        let mut keywords = HashMap::new();
-        keywords.insert("f", TokenType::FUNCTION);
-        keywords.insert("value", TokenType::VALUE);
-        keywords.insert("true", TokenType::TRUE);
-        keywords.insert("false", TokenType::FALSE);
-        keywords.insert("if", TokenType::IF);
-        keywords.insert("else", TokenType::ELSE);
-        keywords.insert("return", TokenType::RETURN);
-        keywords.insert("is", TokenType::EQ);
-        keywords.insert("not", TokenType::NOT_EQ);
-        keywords.insert("end", TokenType::CLOSEBLOCK);
-        keywords.insert("update", TokenType::UPDATE);
-        keywords.insert("constant", TokenType::CONSTANT);
-        keywords.insert("and", TokenType::AND);
-        keywords.insert("or", TokenType::OR);
-        keywords
-    };
+    LBracket,
+    RBracket,
+    LParen,
+    RParen,
+    LBrace,
+    RBrace,
+
+    Ampersand,
+    DoubleAmpersand,
+    Equal,
+    NotEqual,
+    Bar,
+    DoubleBar,
+    Assign,
+
+    True,
+    False,
+    Value
+
 }
 
 #[derive(Debug)]
-pub enum TokenType {
-    // Endings
-    ILLEGAL,
-    EOF,
-    // Identifiers + literals
-    IDENT,
-    INT(usize),
-    // Operators
-    ASSIGN,
-    PLUS,
-    EQ,
-    NOT_EQ,
-
-    // Delimiters
-    COMMA,
-    SEMICOLON,
-    LPAREN,
-    RPAREN,
-    LBRACE,
-    RBRACE,
-    LBRACKET,
-    RBRACKET,
-    COLON,
-    // Keywords
-    FUNCTION,
-    VALUE,
-    UPDATE,
-    CONSTANT,
-    TRUE,
-    FALSE,
-    IF,
-    ELSE,
-    RETURN,
-    STRING(String),
-    MINUS,
-    BANG,
-    ASTERISK,
-    SLASH,
-    LT,
-    GT,
-    LTEQ,
-    GTEQ,
-    INCREMENT,
-    DECREMENT,
-    MINUSEQ,
-    PLUSEQ,
-    MULEQ,
-    SLASHEQ,
-    OR,
-    AND,
-    OPENBLOCK,
-    CLOSEBLOCK,
+pub struct Token {
+    position: Position,
+    kind: TokenType
 }
 
-use TokenType::*;
-
-pub struct Token<'a> {
-    Type: TokenType,
-    literal: &'a str,
-    line: usize,
-    column: usize,
-
+#[derive(Debug, Clone)]
+pub enum LexerError {
+    UndefinedError(Position),
+    ExpectPeek(Position, TokenType, TokenType)
 }
 
-impl<'a> fmt::Display for Token<'a> {
-    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        write!(f, "({}, {}) [{:?}] lit: '{}'", self.line, self.column, self.Type, self.literal)
-    }
+#[derive(Debug, Clone)]
+pub(crate) struct Lexer<'a> {
+    input: &'a str,
+    literal_position: Position,
+    position: usize,
+    errors: Vec<LexerError>
 }
 
-pub fn lookup_ident(identifier: &str) -> Option<&'static TokenType> {
-   KEYWORDS.get(identifier)
-}
+impl<'a> Lexer<'a> {
 
-pub struct LexerError {
-    line: usize,
-    column: usize,
-    reason: String,
-}
-pub struct Lexer {
-    pub input: String,
-    pub position: usize,
-    pub reading_position: usize,
-    pub column: usize,
-    pub line: usize,
-    pub current_char: char,
-    pub errors: Vec<LexerError>
-}
-
-impl Lexer {
-    fn next_token(&mut self) -> Result<Token, String> {
-        let t: Token;
-
-        self.skipWhitespace();
-
-        match self.current_char {
-            '(' => t = self.newCharToken(LPAREN),
-            ')' => t = self.newCharToken(RPAREN),
-            '+' => t = self.newCharToken(PLUS),
-            '-' => {
-                if self.peek_character() == '>' {
-                    t = self.newCharToken(OPENBLOCK)
-                } else {
-
-                }
-            }
-            ',' => t = self.newCharToken(COMMA),
-            '{' => t = self.newCharToken(LBRACKET),
-            _ => {
-                if self.current_char.is_numeric() {
-                    let int_str = self.consumeInteger();
-                    let int = int_str.parse::<usize>().unwrap();
-
-                    t.literal = int_str;
-                    t.Type = INT(int);
-                } else if self.current_char.is_alphabetic() {
-                    let start_col = self.column;
-                    let start_line = self.line;
-                    let str_literal = self.consumeIdentifier().clone();
-                    let TT = lookup_ident(str_literal);
-                    match TT {
-                        Some(&TT) => {
-                            t = Token {
-                                line: start_line,
-                                column: start_col,
-                                literal: str_literal,
-                                Type: TT
-                            }
-                        },
-                        None => {
-                            t = Token {
-                                line: start_line,
-                                column: start_col,
-                                literal: str_literal,
-                                Type: IDENT
-                            }
-                        }
-                    };
-                } else if self.current_char.is_numeric() {
-                    let start_col = self.column;
-                    let start_line = self.line;
-                    let str_literal = self.consumeInteger().clone();
-                    let int = str_literal.parse::<usize>();
-
-                    match int {
-                        Ok(i) => t = Token {
-                            line: start_line,
-                            column: start_col,
-                            literal: str_literal,
-                            Type: INT(i)
-                        },
-                        Err(z) => {
-                            self.add_error(format!("({}, {}) - {}", self.line, self.column, z));
-                            return Result::Err(format!("({}, {}) - {}", self.line, self.column, z));
-                        }
-                    }
-                    
-                        
-                } else {
-                    self.add_error(format!("({}, {}) - {}", self.line, self.column, "illegal token detected."));
-                    return Result::Err(format!("({}, {}) - {}", self.line, self.column, "illegal token detected."));
-                }
-            }
-
-        }
-
-        self.consume_char();
-
-        return Result::Ok(t)
-    }
-
-    pub fn consume_char(&mut self) {
-        if self.reading_position >= self.input.len() {
-            self.current_char = 0 as char;
-        } else {
-            self.current_char = self.input.chars().nth(self.reading_position).unwrap();
-        }
-
+    fn consume_char(&mut self) {
+        self.literal_position.column += 1;
         self.position += 1;
-        self.reading_position += 1;
-        self.column += 1;
+
+
     }
-
-    fn skipWhitespace(&mut self) {
-        while self.current_char.is_ascii_whitespace() {
-            if self.current_char == '\n' {
-                self.line += 1;
-                self.column = 1;
-            }
-            self.consume_char();
-        }
-    }
-
-    fn consumeInteger(&mut self) -> &str {
-        let pos = self.position;
-        while self.current_char.is_ascii_digit() {
-            self.consume_char()
-        }
-
-        return &self.input[pos..self.position];
-    }
-
-    fn consumeIdentifier<'a>(&'a mut self) -> &str {
-        let pos = self.position;
-        while self.current_char.is_alphabetic() {
-            self.consume_char()
-        }
-
-        return &self.input[pos..self.position];
-    }
-
-    fn readString(&mut self) -> &str {
-        let pos = self.position;
-        loop {
-            self.consume_char();
-            if self.current_char == '"' || self.current_char == 0 as char {
-                break;
-            }
-        }
-
-        return &self.input[pos..self.position];
-    }
-
-    fn peek_character(&self) -> char {
-        if self.reading_position >= self.input.len() {
-            return 0 as char;
+    
+    fn get_nth_char(&self, position: Option<usize>) ->  Option<char> {
+        if let Some(pos) = position { //Check if pos exists
+            return self.input.chars().nth(pos)
         } else {
-            return self.input.chars().nth(self.reading_position).unwrap();
+            return self.input.chars().nth(self.position)
         }
     }
 
-    fn newCharToken<'a>(&'a self, Type: TokenType) -> Token<'a> {
-        Token {
-            column: self.column,
-            line: self.line,
-            literal: self.current_char.to_string().as_str(),
-            Type: Type
-        }
+    fn peek_char(&self) -> Option<char> {
+        self.get_nth_char(Some(self.position + 1))
     }
 
-    fn add_error(&mut self, reason: String) {
-        let mut owned = "LEXER ERROR: ".to_owned();
-        owned.push_str(&reason);
-        let err = LexerError {
-            reason: owned,
-            line: self.line,
-            column: self.column
+    pub fn single_char_tok(&mut self, kind: TokenType) -> Option<Token> {
+        let current_position = self.literal_position;
+        self.consume_char();
+        Some(Token{
+            kind: kind,
+            position: current_position
+        })
+    }
+
+    pub fn double_char_tok(&mut self, kind: TokenType) -> Option<Token> {
+        let current_position = self.literal_position;
+        self.consume_char();
+        self.consume_char();
+        Some(Token {
+            kind: kind,
+            position: current_position
+        })
+    }
+
+    pub fn string(&mut self) -> Option<Token> {
+        let current_position = self.literal_position;
+        self.consume_char();
+        let mut owned = String::new();
+        while let Some(ch) = self.get_nth_char(None) {
+            match ch {
+                '"' => {
+                    self.consume_char();
+                    break;
+                },
+                '\n' => {
+                    self.literal_position.column = 0;
+                    self.literal_position.line += 1;
+                    owned.push(ch);
+                    self.consume_char();
+                }
+                _ => {
+                    owned.push(ch);
+                    self.consume_char();
+                }
+            }
+        }
+
+        Some(Token {
+            kind: TokenType::String(owned),
+            position: current_position  
+        })
+    }
+
+    pub fn ident(&mut self) -> Option<Token> {
+        let current_position = self.literal_position;
+        let mut owned = String::new();
+
+        while let Some(ch) = self.get_nth_char(None) {
+            match ch {
+                'A'..='Z' | 'a'..='z' | '_' => {
+                    owned.push(ch);
+                    self.consume_char();
+                },
+                _ => break
+            }
+        }
+
+        let kind = match owned.as_str() {
+            "true" => TokenType::True,
+            "false" => TokenType::False,
+            "value" => TokenType::Value,
+            _ => TokenType::Identifier(owned)
         };
 
-        self.errors.push(err);
+        Some(Token{
+            kind: kind,
+            position: current_position
+        })
     }
 
-    fn lex(&mut self) {
-        let tokens = Vec::<Token>::new();
-        while self.position != self.input.len()-1 {
-            let token = self.next_token();
+    pub fn integer(&mut self) -> Option<Token> {
+        enum NumberTypes {
+            Integer,
+            Float
+        }
+        let current_position = self.literal_position;
+        let mut num = String::new();
+        let mut num_type = NumberTypes::Integer;
+
+        while let Some(ch) = self.get_nth_char(None) {
+            match ch {
+                '0'..='9' => {
+                    num.push(ch);
+                    self.consume_char();
+                },
+                '.' if matches!(self.peek_char(), Some('0'..='9')) => {
+                    num_type = NumberTypes::Float;
+                    num.push(ch);
+                    self.consume_char();
+                },
+                _ => break
+            }
+        }
+
+        match num_type {
+            NumberTypes::Integer => {
+                match num.parse::<usize>() {
+                    Ok(n) => Some(Token {
+                        kind: TokenType::Integer(n),
+                        position: current_position,
+                    }),
+                    _ => {
+                        self.add_error(LexerError::UndefinedError(current_position));
+                        None
+                    }
+                }
+            },
+            NumberTypes::Float => {
+                match num.parse::<f64>() {
+                    Ok(n) => Some(Token {
+                        kind: TokenType::Float(n),
+                        position: current_position,
+                    }),
+                    _ => {
+                        self.add_error(LexerError::UndefinedError(current_position));
+                        None
+                    }
+                }
+            }
+        }
 
 
+    }
+
+    pub fn add_error(&mut self, kind: LexerError) -> Option<Token> {
+        self.errors.push(kind);
+
+        None
+    }
+
+    pub fn errors(self) -> Vec<LexerError> {
+        return self.errors.clone();
+    }
+
+    pub fn new(input: &'a str) -> Lexer<'a> {
+        Self {
+            literal_position: Position {
+                column: 0,
+                line: 1, // Remember: lines aren't zero-indexed.
+            },
+            position: 0,
+            input: input,
+            errors: Vec::new()
         }
     }
 }
 
-fn New(input: String) -> Lexer {
-    Lexer {
-        input: input,
-        position: 0,
-        reading_position: 1,
-        column: 1,
-        line: 1,
-        current_char: input.chars().nth(0).unwrap(),
-        errors: vec![]
+impl<'a> Iterator for Lexer<'a> {
+    type Item = Token;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let current_character = self.get_nth_char(None);
+        let peek_character = self.peek_char();
+
+        if current_character == None {
+            return None
+        }
+
+        match current_character.unwrap() {
+            '(' => self.single_char_tok(TokenType::LParen),
+            ')' => self.single_char_tok(TokenType::RParen),
+            '[' => self.single_char_tok(TokenType::LBracket),
+            ']' => self.single_char_tok(TokenType::RBracket),
+            '{' => self.single_char_tok(TokenType::LBrace),
+            '}' => self.single_char_tok(TokenType::RBrace),
+
+            '=' => {
+                match peek_character {
+                    Some('=') => self.double_char_tok(TokenType::Equal),
+                    None | Some(' ') => self.single_char_tok(TokenType::Assign),
+                    _ => self.add_error(LexerError::UndefinedError(self.literal_position))
+                }
+            },
+            '\n' => {
+                self.literal_position.column = 0;
+                self.literal_position.line += 1;
+                self.consume_char();
+                self.next()
+            },
+            ' ' | '\t' | '\r' => {
+                self.consume_char();
+                self.next()
+            },
+            '0'..='9' => self.integer(),
+            'A'..='Z' | 'a'..='z' | '_' => self.ident(),
+            '"' => self.string(),
+            _ => self.add_error(LexerError::UndefinedError(self.literal_position))
+        }
     }
 }
+
