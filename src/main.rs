1fcdb9d6a1c0b206bcd0e400eb0ca634d1a1e508
Rishi Kothari <itsrishikothari@gmail.com>
2020-11-28 12:21:38 -0500

    update for new tokentypes

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,11 @@ mod util;
 
 fn main() {
     let lex = lexer::Lexer::new(
-        r#"value z = "hello
-waef" 3.45"#);
+        r#"
+value z = "hello waef";
+value x = 3.2444;
+print(z + x);
+"#);
 
     let z: Vec<lexer::Token> = lex.collect();
     println!("{:#?}", z);
