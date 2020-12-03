1bc5de8b32deccab74dc379159e69d674f8adc73
Rishi Kothari <itsrishikothari@gmail.com>
2020-12-03 15:24:27 -0500

    lexer stuff

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,12 +1,12 @@
 #[derive(Debug, Clone, Copy)]
 pub struct Position {
     column: usize,
-    line: usize
+    line: usize,
 }
 
 #[derive(Debug, Clone, PartialEq)]
 pub enum TokenType {
-    //Associated 
+    //Associated
     String(String),
     Integer(usize),
     Float(f64),
@@ -29,6 +29,7 @@ pub enum TokenType {
     Assign,
     Function,
     Bang,
+    Update,
 
     OpenBlock,
     EndBlock,
@@ -51,15 +52,12 @@ pub enum TokenType {
     If,
     Else,
     Return,
-
-
 }
-
 
 #[derive(Debug, Clone)]
 pub struct Token {
     pub position: Position,
-    pub kind: TokenType
+    pub kind: TokenType,
 }
 
 #[derive(Debug, Clone)]
@@ -75,19 +73,17 @@ pub(crate) struct Lexer<'a> {
 }
 
 impl<'a> Lexer<'a> {
-
     fn consume_char(&mut self) {
         self.literal_position.column += 1;
         self.position += 1;
-
-
     }
-    
-    fn get_nth_char(&self, position: Option<usize>) ->  Option<char> {
-        if let Some(pos) = position { //Check if pos exists
-            return self.input.chars().nth(pos)
+
+    fn get_nth_char(&self, position: Option<usize>) -> Option<char> {
+        if let Some(pos) = position {
+            //Check if pos exists
+            return self.input.chars().nth(pos);
         } else {
-            return self.input.chars().nth(self.position)
+            return self.input.chars().nth(self.position);
         }
     }
 
@@ -98,9 +94,9 @@ impl<'a> Lexer<'a> {
     pub fn single_char_tok(&mut self, kind: TokenType) -> Option<Token> {
         let current_position = self.literal_position;
         self.consume_char();
-        Some(Token{
+        Some(Token {
             kind: kind,
-            position: current_position
+            position: current_position,
         })
     }
 
@@ -110,7 +106,7 @@ impl<'a> Lexer<'a> {
         self.consume_char();
         Some(Token {
             kind: kind,
-            position: current_position
+            position: current_position,
         })
     }
 
@@ -123,7 +119,7 @@ impl<'a> Lexer<'a> {
                 '"' => {
                     self.consume_char();
                     break;
-                },
+                }
                 '\n' => {
                     self.literal_position.column = 0;
                     self.literal_position.line += 1;
@@ -139,7 +135,7 @@ impl<'a> Lexer<'a> {
 
         Some(Token {
             kind: TokenType::String(owned),
-            position: current_position  
+            position: current_position,
         })
     }
 
@@ -152,8 +148,8 @@ impl<'a> Lexer<'a> {
                 'A'..='Z' | 'a'..='z' | '_' => {
                     owned.push(ch);
                     self.consume_char();
-                },
-                _ => break
+                }
+                _ => break,
             }
         }
 
@@ -166,19 +162,20 @@ impl<'a> Lexer<'a> {
             "else" => TokenType::Else,
             "f" => TokenType::Function,
             "end" => TokenType::EndBlock,
-            _ => TokenType::Identifier(owned)
+            "update" => TokenType::Update,
+            _ => TokenType::Identifier(owned),
         };
 
-        Some(Token{
+        Some(Token {
             kind: kind,
-            position: current_position
+            position: current_position,
         })
     }
 
     pub fn integer(&mut self) -> Option<Token> {
         enum NumberTypes {
             Integer,
-            Float
+            Float,
         }
         let current_position = self.literal_position;
         let mut num = String::new();
@@ -189,43 +186,42 @@ impl<'a> Lexer<'a> {
                 '0'..='9' => {
                     num.push(ch);
                     self.consume_char();
-                },
+                }
                 '.' if matches!(self.peek_char(), Some('0'..='9')) => {
                     num_type = NumberTypes::Float;
                     num.push(ch);
                     self.consume_char();
-                },
-                _ => break
+                }
+                _ => break,
             }
         }
 
         match num_type {
-            NumberTypes::Integer => {
-                match num.parse::<usize>() {
-                    Ok(n) => Some(Token {
-                        kind: TokenType::Integer(n),
-                        position: current_position,
-                    }),
-                    _ => {
-                        panic!("[{:?}] LEXER ERROR: Could not parse integer.", current_position);
-                        
-                    }
+            NumberTypes::Integer => match num.parse::<usize>() {
+                Ok(n) => Some(Token {
+                    kind: TokenType::Integer(n),
+                    position: current_position,
+                }),
+                _ => {
+                    panic!(
+                        "[{:?}] LEXER ERROR: Could not parse integer.",
+                        current_position
+                    );
                 }
             },
-            NumberTypes::Float => {
-                match num.parse::<f64>() {
-                    Ok(n) => Some(Token {
-                        kind: TokenType::Float(n),
-                        position: current_position,
-                    }),
-                    _ => {
-                        panic!("[{:?}] LEXER ERROR: Could not parse float.", current_position);
-                    }
+            NumberTypes::Float => match num.parse::<f64>() {
+                Ok(n) => Some(Token {
+                    kind: TokenType::Float(n),
+                    position: current_position,
+                }),
+                _ => {
+                    panic!(
+                        "[{:?}] LEXER ERROR: Could not parse float.",
+                        current_position
+                    );
                 }
-            }
+            },
         }
-
-
     }
 
     // pub fn comment(&mut self, block: bool) -> Option<Token> {
@@ -262,7 +258,7 @@ impl<'a> Iterator for Lexer<'a> {
         let peek_character = self.peek_char();
 
         if current_character == None {
-            return None
+            return None;
         }
 
         match current_character.unwrap() {
@@ -273,99 +269,88 @@ impl<'a> Iterator for Lexer<'a> {
             '{' => self.single_char_tok(TokenType::LBrace),
             '}' => self.single_char_tok(TokenType::RBrace),
             ';' => self.single_char_tok(TokenType::Semicolon),
-            '|' => {
-                match peek_character {
-                    Some('|') => self.double_char_tok(TokenType::Or),
-                    None | Some(' ') | Some('\t') | Some('\r') => self.single_char_tok(TokenType::Bar),
-                    _ => panic!("[{:?}] LEXER ERROR: Undefined token", self.literal_position)
-                }
+            '|' => match peek_character {
+                Some('|') => self.double_char_tok(TokenType::Or),
+                None | Some(' ') | Some('\t') | Some('\r') => self.single_char_tok(TokenType::Bar),
+                _ => panic!("[{:?}] LEXER ERROR: Undefined token", self.literal_position),
             },
 
-            '&' => {
-                match peek_character {
-                    Some('&') => self.double_char_tok(TokenType::And),
-                    None | Some(' ') | Some('\t') | Some('\r') => self.single_char_tok(TokenType::Bar),
-                    _ => panic!("[{:?}] LEXER ERROR: Undefined token", self.literal_position)
-                }
+            '&' => match peek_character {
+                Some('&') => self.double_char_tok(TokenType::And),
+                None | Some(' ') | Some('\t') | Some('\r') => self.single_char_tok(TokenType::Bar),
+                _ => panic!("[{:?}] LEXER ERROR: Undefined token", self.literal_position),
             },
 
-            '+' => {
-                match peek_character {
-                    Some('+') => self.double_char_tok(TokenType::DoublePlus),
-                    None | Some(' ') | Some('\t') | Some('\r') => self.single_char_tok(TokenType::Plus),
-                    _ => panic!("[{:?}] LEXER ERROR: Undefined token", self.literal_position)
-                }
+            '+' => match peek_character {
+                Some('+') => self.double_char_tok(TokenType::DoublePlus),
+                None | Some(' ') | Some('\t') | Some('\r') => self.single_char_tok(TokenType::Plus),
+                _ => panic!("[{:?}] LEXER ERROR: Undefined token", self.literal_position),
             },
 
-            '-' => {
-                match peek_character {
-                    Some('-') => self.double_char_tok(TokenType::DoubleMinus),
-                    Some('>') => self.double_char_tok(TokenType::OpenBlock),
-                    None | Some(' ') | Some('\t') | Some('\r') => self.single_char_tok(TokenType::Minus),
-                    _ => panic!("[{:?}] LEXER ERROR: Undefined token", self.literal_position)
+            '-' => match peek_character {
+                Some('-') => self.double_char_tok(TokenType::DoubleMinus),
+                Some('>') => self.double_char_tok(TokenType::OpenBlock),
+                None | Some(' ') | Some('\t') | Some('\r') => {
+                    self.single_char_tok(TokenType::Minus)
                 }
+                _ => panic!("[{:?}] LEXER ERROR: Undefined token", self.literal_position),
             },
 
-            '*' => {
-                match peek_character {
-                    Some('-') => self.double_char_tok(TokenType::DoubleMinus),
-                    Some('>') => self.double_char_tok(TokenType::OpenBlock),
-                    None | Some(' ') | Some('\t') | Some('\r') => self.single_char_tok(TokenType::Minus),
-                    _ => panic!("[{:?}] LEXER ERROR: Undefined token", self.literal_position)
+            '*' => match peek_character {
+                Some('-') => self.double_char_tok(TokenType::DoubleMinus),
+                Some('>') => self.double_char_tok(TokenType::OpenBlock),
+                None | Some(' ') | Some('\t') | Some('\r') => {
+                    self.single_char_tok(TokenType::Minus)
                 }
+                _ => panic!("[{:?}] LEXER ERROR: Undefined token", self.literal_position),
             },
 
             '/' => {
                 match peek_character {
                     // Some('*') => self.double_char_tok(TokenType::OpenBlock),
-                    None | Some(' ') | Some('\t') | Some('\r') => self.single_char_tok(TokenType::Slash),
-                    _ => panic!("[{:?}] LEXER ERROR: Undefined token", self.literal_position)
+                    None | Some(' ') | Some('\t') | Some('\r') => {
+                        self.single_char_tok(TokenType::Slash)
+                    }
+                    _ => panic!("[{:?}] LEXER ERROR: Undefined token", self.literal_position),
                 }
-            },
+            }
 
-            '=' => {
-                match peek_character {
-                    Some('=') => self.double_char_tok(TokenType::Equal),
-                    None | Some(' ') | Some('\t') | Some('\r') => self.single_char_tok(TokenType::Assign),
-                    _ => panic!("[{:?}] LEXER ERROR: Undefined token", self.literal_position)
+            '=' => match peek_character {
+                Some('=') => self.double_char_tok(TokenType::Equal),
+                None | Some(' ') | Some('\t') | Some('\r') => {
+                    self.single_char_tok(TokenType::Assign)
                 }
+                _ => panic!("[{:?}] LEXER ERROR: Undefined token", self.literal_position),
             },
-            '!' => {
-                match peek_character {
-                    Some('=') => self.double_char_tok(TokenType::NotEqual),
-                    None | Some(' ') | Some('\t') | Some('\r') => self.single_char_tok(TokenType::Bang),
-                    _ => panic!("[{:?}] LEXER ERROR: Undefined token", self.literal_position)
-                }
+            '!' => match peek_character {
+                Some('=') => self.double_char_tok(TokenType::NotEqual),
+                None | Some(' ') | Some('\t') | Some('\r') => self.single_char_tok(TokenType::Bang),
+                _ => panic!("[{:?}] LEXER ERROR: Undefined token", self.literal_position),
             },
-            '>' => {
-                match peek_character {
-                    Some('=') => self.double_char_tok(TokenType::GTEq),
-                    None | Some(' ') | Some('\t') | Some('\r') => self.single_char_tok(TokenType::GT),
-                    _ => panic!("[{:?}] LEXER ERROR: Undefined token", self.literal_position)
-                }
+            '>' => match peek_character {
+                Some('=') => self.double_char_tok(TokenType::GTEq),
+                None | Some(' ') | Some('\t') | Some('\r') => self.single_char_tok(TokenType::GT),
+                _ => panic!("[{:?}] LEXER ERROR: Undefined token", self.literal_position),
             },
-            '<' => {
-                match peek_character {
-                    Some('=') => self.double_char_tok(TokenType::LTEq),
-                    None | Some(' ') | Some('\t') | Some('\r') => self.single_char_tok(TokenType::LT),
-                    _ => panic!("[{:?}] LEXER ERROR: Undefined token", self.literal_position)
-                }
+            '<' => match peek_character {
+                Some('=') => self.double_char_tok(TokenType::LTEq),
+                None | Some(' ') | Some('\t') | Some('\r') => self.single_char_tok(TokenType::LT),
+                _ => panic!("[{:?}] LEXER ERROR: Undefined token", self.literal_position),
             },
             '\n' => {
                 self.literal_position.column = 0;
                 self.literal_position.line += 1;
                 self.consume_char();
                 self.next()
-            },
+            }
             ' ' | '\t' | '\r' => {
                 self.consume_char();
                 self.next()
-            },
+            }
             '0'..='9' => self.integer(),
             'A'..='Z' | 'a'..='z' | '_' => self.ident(),
             '"' => self.string(),
-            _ => panic!("[{:?}] LEXER ERROR: Undefined token", self.literal_position)
+            _ => panic!("[{:?}] LEXER ERROR: Undefined token", self.literal_position),
         }
     }
 }
-
