d7eeebfd156f71082a1ab1806a22814825f7c6e9
Rishi Kothari <itsrishikothari@gmail.com>
2020-12-03 15:24:40 -0500

    base

--- src/ast/base.rs
+++ src/ast/base.rs
@@ -1,26 +1,34 @@
 use crate::lexer::{Token, TokenType};
 
-use crate::ast::nodes::{Expression};
+use crate::ast::nodes::Expression;
 
 pub struct PrefixExpression {
     token: Token,
     operator: TokenType,
-    right: Box<dyn Expression>
+    right: Box<dyn Expression>,
 }
 
 impl Expression for PrefixExpression {
-    fn literal(&self) -> String { format!("{:?}", self.token) }
-    fn string(&self) -> String { format!("({:?}{:?})", self.operator, self.token) }
+    fn literal(&self) -> String {
+        format!("{:?}", self.token)
+    }
+    fn string(&self) -> String {
+        format!("({:?}{:?})", self.operator, self.token)
+    }
 }
 
 pub struct InfixExpression {
     token: Token,
     operator: TokenType,
-    right: Box<dyn Expression>
+    right: Box<dyn Expression>,
+    left: Box<dyn Expression>,
 }
 
 impl Expression for InfixExpression {
-    fn literal(&self) -> String { format!("{:?}", self.token) }
-    fn string(&self) -> String { format!("({:?}{:?})", self.operator, self.token) }
+    fn literal(&self) -> String {
+        format!("{:?}", self.token)
+    }
+    fn string(&self) -> String {
+        format!("({:?}{:?})", self.operator, self.token)
+    }
 }
-
