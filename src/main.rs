5fced3f60f7c792454236a04150ee6fe0c872d7b
Rishi Kothari <itsrishikothari@gmail.com>
2020-12-18 18:03:33 -0500

    add parse

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,21 @@
-use parser::Parser;
+// use parser::Parser;
 
 mod ast;
 mod evaluator;
 mod lexer;
 mod objects;
-mod parser;
+mod parser; 
 mod util;
 
 fn main() {
     let lex = lexer::Lexer::new(
         r#"
-        1 + 3 + 4 - 2"#,
+        1 * 2 + 4"#,
     );
 
     let z: Vec<lexer::Token> = lex.collect();
     println!("{:#?}", z);
 
-    let mut p = Parser::new(z);
-    println!("{:#?}", p.parseProgram());
+    // let mut p = Parser::new(z);
+    // println!("{:#?}", p.parseProgram());
 }
